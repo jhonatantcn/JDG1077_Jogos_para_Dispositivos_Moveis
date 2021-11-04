ef7b6b73aa6603df23345e38f03f1c1fc171ffb0
Jhonatan Thallisson Cabral Néry <jhonatanthallisson@gmail.com>
2021-11-04 20:24:44 -0300

    Commit_008
    Aula 11: Acelerômetro e Revisão para a Avaliação 02

---
+++ Assets/Game/Aula11/Exemplo02/Scripts/AccelerometerInput_ExampleTwo.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AccelerometerInput_ExampleTwo : MonoBehaviour
+{
+    public float speed = 200f;
+
+    private bool supports;
+
+    public Vector3 accelerometerValue; // Valor do acelerômetro
+    public Vector3 accelerometerSmoothedValue; // Valor suavizado do acelerômetro 
+
+    void Start()
+    {
+        accelerometerSmoothedValue = Vector3.zero;
+
+        supports = SupportsAccelerometer();
+    }
+
+    public bool SupportsAccelerometer()
+    {
+        if (SystemInfo.supportsAccelerometer)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void Update()
+    {
+        if (supports)
+        {
+            accelerometerValue = Input.acceleration;
+
+            // Suavização da transição entre acelerações (interpolação)
+            accelerometerSmoothedValue = Vector3.Lerp(accelerometerSmoothedValue, accelerometerValue, Time.deltaTime); // Time.deltaTime: tempo de carregamento de um frame a outro, cerca de 0.01 (porém varia)
+
+            // Aplica rotação em z de acordo com a aceleração em x
+            transform.eulerAngles = new Vector3(0, 0, -accelerometerSmoothedValue.x * speed);
+        }
+    }
+}
